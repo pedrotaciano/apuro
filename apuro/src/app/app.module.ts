1035bba2452d08bd55f129b39de620da13c0c129
Pedro Taciano <pedrotms@examroom.ai>
2022-11-21 20:45:40 -0300

    create components

--- apuro/src/app/app.module.ts
+++ apuro/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { CardComponent } from './card/card.component';
+import { CardComponent } from './home-student/card/card.component';
 
 @NgModule({
   declarations: [
